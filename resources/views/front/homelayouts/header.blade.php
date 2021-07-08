276fe6b2ef4440bf0d01912fd1cd0ca931cab59e
Zeynep Nida SARICAN <zsarican997@gmail.com>
2021-07-08 12:43:09 +0300

    yeni sayfalar eklendi

--- resources/views/front/homelayouts/header.blade.php
+++ resources/views/front/homelayouts/header.blade.php
@@ -26,7 +26,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="{{route('homepage')}}">Blog Sitesi </a>
+        <a class="navbar-brand" href="{{route('homepage')}}">NE YAZILIM </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
